a98e6bfb3cb1ab23b9b9f13560ea6708866c7c1b
Jim Palowski and Verna Santos <jim@>
2018-04-04 15:32:32 -0700

    Add check for a single integer

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
-//business logic
-
 var RollDice = function(num){
 var Dice = [];
 var Dicetwo = [];
 var numberone = 1 + Math.floor(Math.random()*6);
 var numbertwo = 1 + Math.floor(Math.random()*6);
+
 Dice.push(numberone);
 Dicetwo.push(numbertwo);
+if(Dice[0] == 1){
+   Dice.push("Oh you lost");
+}
+
+if(Dicetwo[0] == 1){
+   Dicetwo.push("Oh you lost");
+}
+
+
  console.log("Dice one is:", Dice);
  console.log("Dice two is:", Dicetwo);
    $(".diceResult").text(Dice);
