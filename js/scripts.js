7741ffb5c172480535f0a1009d5d75901bab2029
Jim Palowski and vernajs@gmail.com <jim@>
2018-04-05 16:29:51 -0700

    Add Reset Button

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,4 @@
-// var Dice = [];
-// var Dicetwo = [];
-// var numberone = 1 + Math.floor(Math.random()*6);
-// var numbertwo = 1 + Math.floor(Math.random()*6);
-// Dice.push(numberone);
-// Dicetwo.push(numbertwo);
-
+//business logic
 
 function Player() {
   this.score = 0;
@@ -13,33 +7,6 @@ function Player() {
 function Dice(inputNumber){
   this.number = inputNumber;
   }
-
-
-// score.prototype.roundScore = function () {
-//   return this.diceOne + this.diceTwo;
-// };
-
-// Player.prototype.playerScores = function () {
-//   return this.player;
-// };
-//
-// Dice.prototype.newDiceOne = function(num) {
-//   return this.player;
-// }
-
-// var add = function(rollOne, rollTwo){
-//   return rollOne += rollTwo;
-//
-// }
-// if(Dice[0] == 1){
-//    Dice.push("Oh you lost");
-// }
-//
-// if(Dicetwo[0] == 1){
-//    Dicetwo.push("Oh you lost");
-// }
-
-
 
 var playerOne = new Player();
 var playerTwo = new Player();
@@ -60,14 +27,18 @@ $(document).ready(function() {
     $(".diceResult2").text(newDice2.number);
     playerOne.score += newDice.number;
     playerTwo.score += newDice2.number;
-    if(playerOne.score ===1){
-       alert("THis doesnt work")
-    }
+
     $(".playerOne").text(playerOne.score);
     $(".playerTwo").text(playerTwo.score);
+
     setTimeout(function() {
       $(".flashcard").toggleClass('flipped');
     }, 1000);
   });
-
+  // if(playerOne.score ===1){
+  //    alert("THis doesnt work")
+  // }
+  // if(playerTwo.score ===1){
+  //    alert("THis doesnt work")
+  // }
 });
