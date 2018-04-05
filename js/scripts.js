77d6b40fa65a353711cf1e497044d44114d0ad34
Jim Palowski and vernajs@gmail.com <jim@>
2018-04-05 11:45:07 -0700

    Add a score for player one and player two

--- js/scripts.js
+++ js/scripts.js
@@ -5,25 +5,32 @@
 // Dice.push(numberone);
 // Dicetwo.push(numbertwo);
 
-function players(playerOne, playTwo){
-  this.playerOne = playerOne;
-  this.playerTwo = playerTwo;
+function players(selectedplayer){
+this.player = selectedplayer;
 }
 function Dice(inputNumber){
   this.number = inputNumber;
+
+
 }
+
 // score.prototype.roundScore = function () {
 //   return this.diceOne + this.diceTwo;
 // };
 
 players.prototype.playerScores = function () {
-  return this.playerOne + this.playerTwo;
+  return this.player;
 
 };
 
 Dice.prototype.newDiceOne = function() {
-  return this.diceOne;
+  return
 }
+
+// var add = function(rollOne, rollTwo){
+//   return rollOne += rollTwo;
+//
+// }
 // if(Dice[0] == 1){
 //    Dice.push("Oh you lost");
 // }
@@ -51,11 +58,13 @@ console.log(newDice);
      $(".diceResult").text(newDice.number);
      $(".diceResult2").text(newDice2.number);
 console.log("The dice is here:", newDice, newDice2);
-  });
-  $(".players").on('click', function(event) {
-  $(this).toggleClass('flipped');
     event.preventDefault();
-
+  var result = (newDice.number += newDice.number);
+  var result2 = (newDice2.number += newDice2.number);
+  var playerOne = new players(this.player);
+  var playerTwo = new players(this.player);
+  $(".playerOne").text(result);
+  $(".playerTwo").text(result2);
   });
 
 });
